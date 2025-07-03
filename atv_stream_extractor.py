6df5c491b1657fd636eb687c614ded7cabee9e23
MEGA-GROUP <uzagdasan@mail.ru>
2025-07-04 01:47:04 +0400

    Update atv_stream_extractor.py

--- atv_stream_extractor.py
+++ atv_stream_extractor.py
@@ -17,7 +17,7 @@ def get_secure_stream(url):
     try:
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-            'Referer': 'https://www.atv.com.tr/'
+            'Referer': 'https://https://www.atv.com.tr/canli-yayin/'
         }
         
         # 1. Saytın əsas səhifəsini al
